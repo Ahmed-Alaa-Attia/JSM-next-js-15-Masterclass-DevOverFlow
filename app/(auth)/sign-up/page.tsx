055fb1f5afbf0ac2e7cff9b824461b8ccc82be3a
ahmed alaa <ahmedalaa@ahmeds-MacBook-Pro.local>
2025-08-11 01:52:21 +0300

    Form Validation using Zod and shadcn form validation

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
 function SignUp() {
-  return <div>Sign in page</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", username: "", name: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 }
 
 export default SignUp;
