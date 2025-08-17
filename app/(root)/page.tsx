4c12ce5880744e2e86d2a3914822e30498e83f40
ahmed alaa <ahmedalaa@ahmeds-MacBook-Pro.local>
2025-08-18 01:19:50 +0300

    NavBar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const page = async () => {
   const session = await auth();
@@ -11,16 +9,6 @@ const page = async () => {
     <>
       <h1 className="h1-bold">Hello Next.js</h1>
       <h1 className="h1-bold font-space-grotesk">Hello Next.js</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log Out</Button>
-      </form>
     </>
   );
 };
